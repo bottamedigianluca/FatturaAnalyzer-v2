f57a036dfb91b11c90156288b73c50fcea60b0a8
bottamedigianluca <bottamedigianluca@gmail.com>
2025-06-08 18:41:29 +0200

    Update vite.config.ts

--- FatturaAnalyzer-v2/frontend/vite.config.ts
+++ FatturaAnalyzer-v2/frontend/vite.config.ts
@@ -1,30 +1,31 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig({
   plugins: [react()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  // Prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+
+  // Tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
+      // Tell 'vite' to ignore watching 'src-tauri'
       ignored: ["**/src-tauri/**"],
     },
   },
-  // 3. to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
   
+  // To make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ['VITE_', 'TAURI_'],
+
   resolve: {
     alias: {
-      "@": "/src",
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+});
